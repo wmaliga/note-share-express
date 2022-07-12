415f3d993362287c0452c093e9260f6cc68c91a3
wmaliga <wojciech.maliga@gmail.com>
2022-07-12 14:40:13 +0200

    TypeScript Express.js application

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello Word!');
 });
 
