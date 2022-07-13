acc3520385857a0f1c1277d88af471e038aaf4b2
wmaliga <wojciech.maliga@gmail.com>
2022-07-13 09:38:12 +0200

    Parse JSON request body

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,8 +6,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Word!');
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
