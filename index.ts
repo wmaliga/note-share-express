c7f7a7a3a8380dc136e5188cd9c2048140680c49
wmaliga <wojciech.maliga@gmail.com>
2022-07-14 13:41:02 +0200

    CORS allow all

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import {notesRouter} from './src/routes/notes.route';
 
@@ -9,13 +10,14 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Word!');
     res.sendStatus(200);
 });
 
-app.use('/notes', notesRouter);
+app.use('/api/v1/notes', notesRouter);
 
 app.listen(port, () => {
     console.log(`[server] Server is running at: http://localhost:${port}`);
