808c6420f5c1b5401cb8a9e733632e6dc31368b3
wmaliga <wojciech.maliga@gmail.com>
2022-07-15 12:51:42 +0200

    Error handler implemented

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
 import {notesRouter} from './src/routes/notes.route';
+import {HttpError} from "http-errors";
 
 dotenv.config();
 
@@ -12,12 +13,20 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
+const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log('[ErrorHandler] error message:', error.message);
+    const statusCode = error instanceof HttpError ? error.statusCode : 500;
+    res.status(statusCode).send(error.message);
+    next(error);
+};
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Word!');
     res.sendStatus(200);
 });
 
 app.use('/api/v1/notes', notesRouter);
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`[server] Server is running at: http://localhost:${port}`);
--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -1,44 +1,36 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 
 import {Note} from "../models/notes.model";
 import NotesService from "../services/notes.service";
 
 export default class NotesController {
-    static async findPublicNotes(req: Request, res: Response) {
+    static async findPublicNotes(req: Request, res: Response, next: NextFunction) {
         console.log('[NotesController] GET call findPublicNotes');
         NotesService.findPublicNotes().then(notes => {
             res.status(200).json(notes);
-        }).catch(error => {
-            res.status(500).json(error);
-        });
+        }).catch(next);
     }
 
-    static async getNoteType(req: Request, res: Response) {
+    static async getNoteType(req: Request, res: Response, next: NextFunction) {
         console.log('[NotesController] GET call getNoteType');
         NotesService.getNoteType(req.params.id).then(type => {
             res.status(200).json(type);
-        }).catch(error => {
-            res.status(500).json(error);
-        });
+        }).catch(next);
     }
 
-    static async getNote(req: Request, res: Response) {
+    static async getNote(req: Request, res: Response, next: NextFunction) {
         console.log('[NotesController] GET call getNote');
         NotesService.getNote(req.params.id).then(note => {
             res.status(200).json(note);
-        }).catch(error => {
-            res.status(500).json(error);
-        });
+        }).catch(next);
     }
 
-    static async saveNote(req: Request, res: Response) {
+    static async saveNote(req: Request, res: Response, next: NextFunction) {
         console.log('[NotesController] POST call saveNote');
         const note = NotesController.parseNote(req.body);
         NotesService.saveNote(note).then(id => {
             res.status(201).set('Location', id).send();
-        }).catch(error => {
-            res.status(500).json(error);
-        });
+        }).catch(next);
     }
 
     private static parseNote(json: any): Note {
